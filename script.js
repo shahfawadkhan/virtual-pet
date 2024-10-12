b532b728d6afc275262396a6a095816faf5b03ad
shahfawadkhan <you@sfawadkhan92@gmail.com>
2024-10-12 05:47:11 -0700

    add methods

--- script.js
+++ script.js
@@ -4,5 +4,33 @@ var pet  = {
     age : 9,
     happiness : 50 ,
     hunger : 60 ,
+    feed : function(){
+        if(this.hunger >= 20){
+            this.happiness = this.happiness-20;
+            console.log(this.happiness)
+        }
+    },
+    play : function(){
+        if(this.happiness>=0 && this.happiness<=100){
+            this.happiness+=20;
+            console.log(this.happiness)
+        }
+    },
+    agePet : function(){
+        this.age+=1;
+        this.happiness = this.happiness-5;
+        this.hunger+=10;
+
+        // console.log(this.age)
+        // console.log(this.happiness)
+        // console.log(this.hunger)
+
+    },
+    getInfo : function(){
+        console.log(`Pet Name is ${this.name} Type is ${this.type} age is ${this.age} , happiness ${this.happiness} hunger ${this.hunger}`)
+    }
 }
+
+
+
 
